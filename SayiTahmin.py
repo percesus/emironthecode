2326170669b2ca6bf9c373a33e1c84a45d728d76
Emir Yıldız <96476917+percesus@users.noreply.github.com>
2022-03-21 16:04:58 +0300

    Update SayiTahmin.py
    Hata Düzeltme
    
    int değişkeni düzeltildi.
    min-max formatı onarıldı.

--- SayiTahmin.py
+++ SayiTahmin.py
@@ -1,7 +1,10 @@
 package odevhafta3;
 package SayiTahminOdevi
 while True:
-        
+        int Max = 100;
+        int Min = 0;
+
+        int rastgele = (int)(Math.random() * ( Max - Min ))
     sayi = int(input("1 ile 100 arasında bir sayı giriniz. (Oyundan Çıkmak için "0"):"))
     
     tahminSayi += 1
@@ -22,3 +25,4 @@ while True:
         print("Tebrikler Sayıyı Tahmin Ettiniz! Rastgele sayı = {0}".format(rastgeleSayi))
         
         print("{0} Kerede Sayıyı Tahmin Ettiniz.".format(tahminSayi)
+System.out.print(rastgele);
